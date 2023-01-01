4a21bb6b0bf5cb028155766efcd12de5e5507074
Nikos Kechris <nkechris22@gmail.com>
2023-01-02 01:43:38 +0200

    Many changes

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,11 +1,35 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router'
+import { useConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>Greek Postal Codes API</span>,
   footer: {
     text: 'Created by Fourth Ally',
   },
+  feedback: {
+    content: null,
+  },
+  editLink:{
+    component:null
+  },
+  // docsRepositoryBase: 'https://github.com/nkechr1s/postal-codes-doc/tree/main',
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – GPCA'
+    }
+  },
+  head: () => {
+    const { asPath } = useRouter()
+    const { frontMatter } = useConfig()
+    return <>
+      <meta property="og:url" content={`https://greek-postal-codes.gr${asPath}`} />
+      <meta property="og:title" content={frontMatter.title || 'Greek Postal Codes API'} />
+      <meta property="og:description" content={frontMatter.description || 'Get data for all greek postal codes'} />
+    </>
+  },
 }
 
 export default config
+
